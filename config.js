a240b7523217732ac24fbdd66b7aa90dbd87d992
yasser farhadi <yasser.farhadee@gmail.com>
2024-08-23 22:25:08 +0430

    handlers and workers

--- config.js
+++ config.js
@@ -4,12 +4,26 @@ environments.staging = {
   httpPort: 3000,
   httpsPort: 3001,
   envName: 'staging',
+  hashingSecret: 'thisisasecretF',
+  maxChecks: 5,
+  twilio: {
+    accountSid: '',
+    authToken: '',
+    fromPhone: '',
+  },
 };
 
 environments.production = {
   httpPort: 5000,
   httpsPort: 5001,
   envName: 'production',
+  hashingSecret: 'thisisasecret',
+  maxChecks: 5,
+  twilio: {
+    accountSid: '',
+    authToken: '',
+    fromPhone: '',
+  },
 };
 
 const currentEnvironment =
--- index.js
+++ index.js
@@ -1,92 +1,14 @@
-const http = require('node:http');
-const https = require('node:https');
-const url = require('node:url');
-const config = require('./config');
-const fs = require('node:fs');
-const _data = require('./lib/data');
+const server = require('./lib/server');
+const workers = require('./lib/workers');
 
-_data.delete('test', 'hello', function (err) {
-  console.log('this was the error', err);
-});
+const app = {};
 
-const StringDecoder = require('node:string_decoder').StringDecoder;
+app.init = function () {
+  server.init();
 
-const httpServer = http.createServer(function (req, res) {
-  unifiedServer(req, res);
-});
-
-httpServer.listen(config.httpPort, function () {
-  console.log(`The server listening on port ${config.httpPort}.`);
-});
-
-const httpsServerOptions = {
-  key: fs.readFileSync('./https/key.pem'),
-  cert: fs.readFileSync('./https/cert.pem'),
+  workers.init();
 };
 
-const httpsServer = https.createServer(httpsServerOptions, function (req, res) {
-  unifiedServer(req, res);
-});
+app.init();
 
-httpsServer.listen(config.httpsPort, function () {
-  console.log(`The server listening on port ${config.httpsPort}.`);
-});
-
-function unifiedServer(req, res) {
-  const parsedUrl = url.parse(req.url, true);
-
-  const path = parsedUrl.pathname;
-
-  const trimmedPath = path.replace(/^\/+|\/+$/g, '');
-
-  const method = req.method.toLowerCase();
-  const query = parsedUrl.query;
-  const headers = req.headers;
-
-  const decoder = new StringDecoder('utf-8');
-
-  let buffer = '';
-
-  req.on('data', function (data) {
-    buffer += decoder.write(data);
-  });
-
-  req.on('end', function () {
-    buffer += decoder.end();
-
-    const handler = handlers[trimmedPath] || handlers.notFound;
-    const data = {
-      trimmedPath,
-      queryStringObject: query,
-      method,
-      headers,
-      payload: buffer,
-    };
-
-    handler(data, function (statusCode, payload) {
-      statusCode = typeof statusCode === 'number' ? statusCode : 200;
-      payload = typeof payload === 'object' ? payload : {};
-
-      const stringPayload = JSON.stringify(payload);
-
-      res.setHeader('Content-Type', 'application/json');
-      res.writeHead(statusCode);
-      res.end(stringPayload);
-      console.log(`Returning this response: ${statusCode} ${stringPayload}`);
-    });
-  });
-}
-
-const handlers = {};
-
-handlers.ping = function (_data, callback) {
-  callback(200);
-};
-
-handlers.notFound = function (data, callback) {
-  callback(404);
-};
-
-const router = {
-  ping: handlers.ping,
-};
+module.exports = app;
